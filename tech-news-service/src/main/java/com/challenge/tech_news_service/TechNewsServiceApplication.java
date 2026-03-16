dbbf5657af8a18c1eb6151948557d22bba2367fe
jerryalex15 <jerryrazafindraibe@gmail.com>
2026-03-16 12:44:54 +0100

    first commit

---
+++ tech-news-service/src/main/java/com/challenge/tech_news_service/TechNewsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.tech_news_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechNewsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechNewsServiceApplication.class, args);
+	}
+
+}
---
+++ tech-news-service/src/test/java/com/challenge/tech_news_service/TechNewsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.tech_news_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechNewsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
